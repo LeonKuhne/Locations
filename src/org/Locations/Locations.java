9d89e0c9d5bd14c14d6e9e96e2610533f1b4b4e8
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 23:03:19 -0400

    [auto] build save

--- src/org/Locations/Locations.java
+++ src/org/Locations/Locations.java
@@ -3,11 +3,13 @@ package org.Locations;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.lang.reflect.Field;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.CommandMap;
 
 /**
  * @author leee leee
