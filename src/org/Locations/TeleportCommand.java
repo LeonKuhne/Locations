6c70e53c83e73c3870ff1b6ff699d855a54c43e4
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 23:14:56 -0400

    [auto] build save

--- src/org/Locations/TeleportCommand.java
+++ src/org/Locations/TeleportCommand.java
@@ -1,7 +1,15 @@
 package org.Locations;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.command.PluginIdentifiableCommand;
+
 /**
  * @author leee leee
  */
 public class TeleportCommand implements PluginIdentifiableCommand {
+    
+    public TeleportCommand(String name, Plugin plugin) {
+        super(name, plugin);
+    }
+
 }
