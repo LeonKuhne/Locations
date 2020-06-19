a96cf9a5111e99a4bd3f1fa0c1cffee50391a0cc
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 10:21:01 -0400

    [auto] build save

--- src/org/Locations/Locations.java
+++ src/org/Locations/Locations.java
@@ -18,7 +18,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Locations extends JavaPlugin {
    
-    private final int HELP_COMMAND_WIDTH = 80; // character width spacing for the help command menu
+    private final int HELP_COMMAND_PADDING = 40; // character width spacing for the help command menu
     private HashMap<String, String> helpDesc = new HashMap() {{
         put("/locs", "list available locations");
         put("/locs set/add [name]", "creates a new location");
@@ -172,7 +172,7 @@ public class Locations extends JavaPlugin {
         for (Map.Entry<String, String> entry : helpDesc.entrySet()) {
             String helpCmd = entry.getKey();
             String helpDesc = entry.getValue();
-            helpDesc += new String(new char[HELP_COMMAND_WIDTH-helpCmd.length()]).replace("\0", " "); // add padding
+            helpDesc += new String(new char[HELP_COMMAND_PADDING - helpCmd.length()]).replace("\0", " "); // add padding
 
             help(player, ChatColor.AQUA + helpDesc + ChatColor.GREEN + helpCmd);
         }
