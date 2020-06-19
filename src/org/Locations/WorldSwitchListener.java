f4b5360040a0724143666fce2f444d85e2013466
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 10:46:43 -0400

    [auto] build save

--- src/org/Locations/WorldSwitchListener.java
+++ src/org/Locations/WorldSwitchListener.java
@@ -6,7 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 /**
  * @author leee leee
