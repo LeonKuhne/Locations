6af4e5e16bd191742008734a3501f8c913c5b085
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 08:28:12 -0400

    [auto] build save

--- src/org/Locations/Util.java
+++ src/org/Locations/Util.java
@@ -1,11 +1,41 @@
 
-
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class Util {
+
+    private File file;
+    private FileConfiguration config;
     
-    static Map<String, Command> loadShortcuts() {
-    
+    public static Map<String, Command> loadShortcuts(String dir) {
+        file = new File(dir, "leeslocs.yml");
+        if (file.exists()) {
+            try {
+                config.load(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    public static void saveShortcuts(String dir) {
+        
+        file = new File(dir, "leeslocs.yml");
+        if (!file.exists()) {
+
+            // create
+            file.getParentFile().mkdirs();
+            saveResource("custom.yml", false);
+
+        } else {
+
+            // update
+            
+        
+        }
     }
     
 }
