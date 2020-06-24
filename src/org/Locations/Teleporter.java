3d745561ff44ed8e294876baa56bb89e00fbd6fa
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 12:02:00 -0400

    [auto] build save

--- src/org/Locations/Teleporter.java
+++ src/org/Locations/Teleporter.java
@@ -32,7 +32,7 @@ public class Teleporter {
         shortcuts = new HashMap();
         lastLocs = new HashMap();
 
-        Util.loadShortcuts();
+        Util.loadShortcuts(plugin);
         updateWorlds();
     }
 
