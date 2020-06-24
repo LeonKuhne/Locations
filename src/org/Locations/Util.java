757dc8a530b26dbdf64fde752b85064ab4cb46a2
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 11:34:05 -0400

    [auto] build save

--- src/org/Locations/Util.java
+++ src/org/Locations/Util.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.command.Command;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.command.CommandMap;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
 import java.lang.reflect.Field;
 
