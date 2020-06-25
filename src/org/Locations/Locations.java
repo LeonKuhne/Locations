dbacf1fda1c497133cf2cd09584da89656ff348c
BuildTools <unconfigured@null.spigotmc.org>
2020-06-25 08:44:47 -0400

    [auto] build save

--- src/org/Locations/Locations.java
+++ src/org/Locations/Locations.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 
 /**
  * @author leee leee
